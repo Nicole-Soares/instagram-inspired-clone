95394845b85d015846ecf3f2a2caa774ebb20718
Nicole-Soares <nicolesoares918@gmail.com>
2025-11-16 17:52:08 -0300

    arreglos

--- Mobile/app/_layout.jsx
+++ Mobile/app/_layout.jsx
@@ -1,19 +1,24 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import 'react-native-reanimated';
 import { FollowProvider } from "../hooks/followContext";
+
+export const unstable_settings = { anchor: '(tabs)' };
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <FollowProvider>
-        <Stack
-          screenOptions={{
-            headerTitleAlign: "left",
-            headerShadowVisible: false,
-            headerStyle: { backgroundColor: "#fff" },
-            headerTintColor: "#111827",
-          }}
-        >
+      <Stack
+  screenOptions={{
+    headerTitleAlign: "left",
+    headerShadowVisible: false,
+    headerStyle: { backgroundColor: "#fff" },
+    headerTintColor: "#111827",
+  }}
+>
+
           <Stack.Screen name="(tabs)" />
           <Stack.Screen name="users" />
           <Stack.Screen name="post" />
@@ -23,6 +28,7 @@ export default function RootLayout() {
             options={{ presentation: "transparentModal", headerShown: false }}
           />
         </Stack>
+        <StatusBar style="dark" backgroundColor="#fff" />
       </FollowProvider>
     </GestureHandlerRootView>
   );
